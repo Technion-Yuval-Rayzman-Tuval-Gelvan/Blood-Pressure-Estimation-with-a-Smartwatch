be533a811f3e3b29ca2e81bd61423ed15ca6504a
Tuval198 <tuvalgelvan@staff.technion.ac.il>
2022-09-15 15:04:16 +0000

    LDA model support.

--- Code/Preprocessing/Project_B/LDA.py
+++
@@ -1,9 +0,0 @@
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-## %%%%%%%%%%%%%%% Your code here - Begin %%%%%%%%%%%%%%%
-classifier = LinearDiscriminantAnalysis()
-classifier.fit(x_train_shaped,y_train_shaped.ravel())
-h_func = lambda x: classifier.predict(x.reshape(x.shape[0], -1))
-## %%%%%%%%%%%%%%% Your code here - End %%%%%%%%%%%%%%%%%
-
-print(f'The score on the validation set is: {calc_score(h_func, x_val, y_val):.4f}')
